72a6a7a36744864465c0c6651ef25ce69f4ccbab
MarwanBz <kaka.mer1998@gmail.com>
2023-11-27 11:11:43 +0300

    initail commit

---
+++ todo_app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import {TodoList}  from './components/TodoList'
+import reactLogo from './assets/react.svg'
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>HELLO</h1>
+      <TodoList />
+    </>
+  )
+}
+
+export default App
